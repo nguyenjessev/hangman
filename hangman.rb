e90aa701650b56315b1a465b20beee6290d85516
Jesse <nguyenjessev@gmail.com>
2020-08-16 10:37:07 -0400

    Create initialize and generate_secret_word methods

--- hangman.rb
+++ hangman.rb
@@ -28,5 +28,28 @@ module Hangman
   # This class represents the overall game state
   class Game
     include Serializable
+
+    attr_accessor :secret_word, :revealed_letters, :guessed_letters, :lives_left, :possible_words
+
+    def initialize
+      @secret_word = ''
+      @revealed_letters = ''
+      @guessed_letters = []
+      @lives_left = 0
+      @possible_words = File.readlines('5desk.txt', chomp: true).select { |word| word.length.between?(5, 12) }
+    end
+
+    def start_game
+      generate_secret_word
+      self.guessed_letters = []
+      self.lives_left = 6
+    end
+
+    private
+
+    def generate_secret_word
+      self.secret_word = possible_words.sample
+      self.revealed_letters = '_' * secret_word.length
+    end
   end
 end
